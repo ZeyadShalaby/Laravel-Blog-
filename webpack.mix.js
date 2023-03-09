0f182ccfc9950f2b561f3989d70ae9215da8c7ab
ZeyadShalaby <104474463+ZeyadShalaby@users.noreply.github.com>
2023-03-09 12:31:25 +0200

    Final commit??

---
+++ webpack.mix.js
@@ -0,0 +1,17 @@
+const mix = require("laravel-mix");
+
+mix.styles(
+    [
+        "node_modules/bootstrap/dist/css/bootstrap.min.css",
+        "resources/css/app.css",
+    ],
+    "public/css/app.css"
+).version();
+
+mix.js(
+    [
+        "resources/js/app.js"
+    ],
+    "public/js/app.js"
+).version();
+
